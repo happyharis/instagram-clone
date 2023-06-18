a93761019451de682aaba21cfa7ee995d71738e9
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-18 13:52:19 +0800

    Add insta icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 export default function App() {
   return (
     <Row>
-      <Col sm={1}>Sidebar</Col>
+      <Col
+        sm={1}
+        className="d-flex flex-column justify-content-start align-items-center vh-100 bg-light"
+        style={{ position: "sticky", top: 0 }}
+      >
+        <Button variant={"light"} style={{ marginBottom: "7px" }}>
+          <i className={"bi bi-instagram"} style={{ "font-size": "24px" }}></i>
+        </Button>
+      </Col>
       <Col sm={11}>Main</Col>
     </Row>
   );

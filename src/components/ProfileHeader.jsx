b114e305b0bf0f4e3c436c1e1b5ebc0198cc1945
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-18 15:33:30 +0800

    Add profile data

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,14 +1,13 @@
+import { useContext } from "react";
 import { Col, Image, Row } from "react-bootstrap";
+import { ProfileContext } from "../App";
 
 export default function ProfileHeader() {
+  const { image } = useContext(ProfileContext);
   return (
     <Row className="p-5">
       <Col md={3} className="d-flex align-items-center justify-content-center">
-        <Image
-          src={"https://sig1.co/logo-1"}
-          style={{ height: "150px" }}
-          roundedCircle
-        />
+        <Image src={image} style={{ height: "150px" }} roundedCircle />
       </Col>
       <Col md={9}>Profile Description</Col>
     </Row>

ee819394e071472c2aa6981120b12b8c96576415
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-18 13:58:41 +0800

    Add sidebar buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,35 @@ export default function App() {
         className="d-flex flex-column justify-content-start align-items-center vh-100 bg-light"
         style={{ position: "sticky", top: 0 }}
       >
-        <Button variant={"light"} style={{ marginBottom: "7px" }}>
-          <i className={"bi bi-instagram"} style={{ "font-size": "24px" }}></i>
+        <Button variant="light my-4" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-instagram" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-house" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-search" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-compass" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-film" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-chat" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-heart" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-plus-square" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-person-circle" style={{ fontSize: "24px" }}></i>
+        </Button>
+        <Button variant="light mt-auto mb-3" style={{ marginBottom: "7px" }}>
+          <i className="bi bi-list" style={{ fontSize: "24px" }}></i>
         </Button>
       </Col>
       <Col sm={11}>Main</Col>
